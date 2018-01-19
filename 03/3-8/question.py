f094129d0707f0d219743ec8faa6274efb5bea22
Kento Okamura <kentooooo.1230@gmail.com>
2018-01-20 01:19:30 +0900

    3章 Py の具:リスト、タプル、辞書、集合

---
+++ 03/3-8/question.py
@@ -0,0 +1,29 @@
+# 3-1
+
+years_list = [1993, 1994, 1995, 1996, 1997]
+
+# 3-2
+
+print(years_list[3])
+
+# 3-3
+
+print(years_list[-1])
+
+# 3-4
+
+things = ["mozzarella","cinderella","selmonella"]
+
+# 3-5
+print(things[1].capitalize())
+print(things)
+
+# 3-6
+print(things[0].upper())
+
+# 3-7
+print(things.remove("selmonella"))
+
+surprise = ['Groucho', 'Chico', 'Harpo']
+
+surprise[-1].lower()
