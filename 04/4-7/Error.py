aabd51e16688dbe7b99dfec8b4472db3d38b4afa
Kento Okamura <kentooooo.1230@gmail.com>
2018-01-23 01:10:01 +0900

    4章 Pyの皮: コード構造

--- 04/4-7/Error.py
+++ 04/4-7/Error.py
@@ -4,3 +4,23 @@ try:
     short_list[position]
 except:
     print('Need a position between 0 and', len(short_list)-1, 'but got',position)
+
+while True:
+    value = input('Position [q to quit]? ')
+    if value == 'q':
+        break
+    try:
+        position = int(value)
+        print(short_list[position])
+    except IndexError as err:
+        print('Bad index:',position)
+    except Exception as other:
+        print('Something else brole:',other)
+
+class UppercaseException(Exception):
+    pass
+
+words = ['eeenie', 'meenie', 'miny', 'MO']
+for word in words:
+    if word.isupper():
+        raise UppercaseException(word)
